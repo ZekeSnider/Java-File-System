201325204d5916e34b5b55b24219e6cbd4c0add0
Zeke Snider <eksnider@uw.edu>
2015-03-17 01:01:33 -0700

    bug fix in filesystem.write

--- FileSystem.java
+++ FileSystem.java
@@ -135,6 +135,9 @@ public class FileSystem
 	// [FINISHED]
 	int write(FileTableEntry fd, byte[] buffer)
 	{
+		if (fd==null)
+			return -1;
+		
 		// check if reading only
 		if (fd.mode == "r") {
 			return -1;
